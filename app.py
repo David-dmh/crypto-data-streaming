0fce98c54c952cf59eea021b0f6721b7fc6eddcf
David-dmh <dvd.mark.h@gmail.com>
2022-11-21 19:09:17 +0200

    Add basic nav struct

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, request, url_for, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route('/Home', methods=['GET'])
-def home_url():
-    """"""
+@app.route("/Home")
+def Home():
     return render_template("Home.html")
-  
 
-@app.route('/Listings', methods=['GET'])
-def listings_url():
-    """"""
+@app.route("/Listings", methods=["GET", "POST"])
+def Listings():
+    if request.method == "POST":
+        return redirect(url_for("Home"))
 
+    return render_template("Listings.html")
 
+@app.route("/AnalyticsSuite", methods=["GET", "POST"])
+def AnalyticsSuite():
+    if request.method == "POST":
+        return redirect(url_for("Home"))
 
-@app.route('/AnalyticsSuite', methods=['GET'])
-def analyticssuite_url():
-    """"""
+    return render_template("AnalyticsSuite.html")
 
-
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
