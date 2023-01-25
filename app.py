f1101bcf98b9e2cf189a1b49ef09c7fa7db32e2b
David-dmh <dvd.mark.h@gmail.com>
2023-01-25 22:05:25 +0200

    add user class

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, url_for
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.secret_key = "123"
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.get(user_id)
+
+
+class User(db.model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
 
 @app.route("/")
 def Home():
