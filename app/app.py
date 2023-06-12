6d1376d09c4f7a06decaf44cc9182d2dda6eb24d
DavidHerbert <dvd.mark.h@gmail.com>
2023-06-12 21:45:13 +0200

    test commit

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
+import sys
 from requests import session
 from flask import abort, jsonify, redirect, render_template, request, url_for
 from flask_migrate import Migrate
-import sys
 import cryptocompare
 
 from init import create_app
